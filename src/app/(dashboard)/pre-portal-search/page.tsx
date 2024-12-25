a43ac37e946b5963922087bdbed882ecaa778ff2
Muhammad Farhan <m.farhankz@gmail.com>
2024-12-25 08:15:43 +0500

    added player list

--- src/app/(dashboard)/pre-portal-search/page.tsx
+++ src/app/(dashboard)/pre-portal-search/page.tsx
@@ -1,7 +1,385 @@
 "use client";
 
+import { Flex, Typography } from "antd";
+import Image from "next/image";
+// import Link from "next/link";
 export default function PrePortalSearch() {
-    return(
-        <div>Pre Portal Search</div>
-    );
+  return (
+    <Flex>
+      <div className="main-container">
+        <div className="grid grid-cols-12 gap-4 mb-4">
+          <div className="col-span-9 card">
+            1
+            <div className="player-img">
+              <Image src="/plyer-b.png" alt="abc" width={250} height={250} />
+              <ul>
+                <li>
+                  <i className="icon-teacher"></i> Jackson HS
+                </li>
+                <li>
+                  <i className="icon-receipt-item"></i> 6&apos;2&quot;, 225 lbs
+                </li>
+                <li>
+                  <i className="icon-calendar-1"></i> RFR
+                </li>
+                <li>
+                  <i className="icon-location"></i> New York, NY
+                </li>
+              </ul>
+            </div>
+            <div className="detail-box">
+              <span>Active</span>ID AX30902
+              <h3>Bryce Shaun</h3>
+              <div className="rating">4.2</div>
+            </div>
+          </div>
+          <div className="col-span-3 card">2</div>
+
+          <div className="col-span-8 card">1</div>
+          <div className="col-span-4 card">2</div>
+
+          <div className="col-span-12 card">
+          <Typography.Title level={2}>Similar Players</Typography.Title>
+            <div className="grid grid-cols-2">
+            
+              <div className="players-list">
+                <Typography.Title level={3}>
+                  New Joined Players
+                </Typography.Title>
+                <ul>
+                  <li>
+                    <Image
+                      src="/plyer-b.png"
+                      alt="abc"
+                      width={80}
+                      height={80}
+                    />
+                    <div className="ml-2">
+                      <Typography.Title level={5}>
+                        <span>Ralph</span> Edwards <i>99.3</i>
+                      </Typography.Title>
+                      <Typography.Paragraph>
+                        Senior Team Manager
+                      </Typography.Paragraph>
+                      <Typography.Paragraph>
+                        <Image src="/b.svg" alt="abc" width={15} height={18} />
+                        Johns Hopkins University
+                      </Typography.Paragraph>
+                    </div>
+                  </li>
+                  <li>
+                    <Image
+                      src="/plyer-b.png"
+                      alt="abc"
+                      width={80}
+                      height={80}
+                    />
+                    <div className="ml-2">
+                      <Typography.Title level={5}>
+                        <span>Ralph</span> Edwards <i>99.3</i>
+                      </Typography.Title>
+                      <Typography.Paragraph>
+                        Senior Team Manager
+                      </Typography.Paragraph>
+                      <Typography.Paragraph>
+                        <Image src="/b.svg" alt="abc" width={15} height={18} />
+                        Johns Hopkins University
+                      </Typography.Paragraph>
+                    </div>
+                  </li>
+                  <li>
+                    <Image
+                      src="/plyer-b.png"
+                      alt="abc"
+                      width={80}
+                      height={80}
+                    />
+                    <div className="ml-2">
+                      <Typography.Title level={5}>
+                        <span>Ralph</span> Edwards <i>99.3</i>
+                      </Typography.Title>
+                      <Typography.Paragraph>
+                        Senior Team Manager
+                      </Typography.Paragraph>
+                      <Typography.Paragraph>
+                        <Image src="/b.svg" alt="abc" width={15} height={18} />
+                        Johns Hopkins University
+                      </Typography.Paragraph>
+                    </div>
+                  </li>
+                  <li>
+                    <Image
+                      src="/plyer-b.png"
+                      alt="abc"
+                      width={80}
+                      height={80}
+                    />
+                    <div className="ml-2">
+                      <Typography.Title level={5}>
+                        <span>Ralph</span> Edwards <i>99.3</i>
+                      </Typography.Title>
+                      <Typography.Paragraph>
+                        Senior Team Manager
+                      </Typography.Paragraph>
+                      <Typography.Paragraph>
+                        <Image src="/b.svg" alt="abc" width={15} height={18} />
+                        Johns Hopkins University
+                      </Typography.Paragraph>
+                    </div>
+                  </li>
+                  <li>
+                    <Image
+                      src="/plyer-b.png"
+                      alt="abc"
+                      width={80}
+                      height={80}
+                    />
+                    <div className="ml-2">
+                      <Typography.Title level={5}>
+                        <span>Ralph</span> Edwards <i>99.3</i>
+                      </Typography.Title>
+                      <Typography.Paragraph>
+                        Senior Team Manager
+                      </Typography.Paragraph>
+                      <Typography.Paragraph>
+                        <Image src="/b.svg" alt="abc" width={15} height={18} />
+                        Johns Hopkins University
+                      </Typography.Paragraph>
+                    </div>
+                  </li>
+                  <li>
+                    <Image
+                      src="/plyer-b.png"
+                      alt="abc"
+                      width={80}
+                      height={80}
+                    />
+                    <div className="ml-2">
+                      <Typography.Title level={5}>
+                        <span>Ralph</span> Edwards <i>99.3</i>
+                      </Typography.Title>
+                      <Typography.Paragraph>
+                        Senior Team Manager
+                      </Typography.Paragraph>
+                      <Typography.Paragraph>
+                        <Image src="/b.svg" alt="abc" width={15} height={18} />
+                        Johns Hopkins University
+                      </Typography.Paragraph>
+                    </div>
+                  </li>
+                  <li>
+                    <Image
+                      src="/plyer-b.png"
+                      alt="abc"
+                      width={80}
+                      height={80}
+                    />
+                    <div className="ml-2">
+                      <Typography.Title level={5}>
+                        <span>Ralph</span> Edwards <i>99.3</i>
+                      </Typography.Title>
+                      <Typography.Paragraph>
+                        Senior Team Manager
+                      </Typography.Paragraph>
+                      <Typography.Paragraph>
+                        <Image src="/b.svg" alt="abc" width={15} height={18} />
+                        Johns Hopkins University
+                      </Typography.Paragraph>
+                    </div>
+                  </li>
+                  <li>
+                    <Image
+                      src="/plyer-b.png"
+                      alt="abc"
+                      width={80}
+                      height={80}
+                    />
+                    <div className="ml-2">
+                      <Typography.Title level={5}>
+                        <span>Ralph</span> Edwards <i>99.3</i>
+                      </Typography.Title>
+                      <Typography.Paragraph>
+                        Senior Team Manager
+                      </Typography.Paragraph>
+                      <Typography.Paragraph>
+                        <Image src="/b.svg" alt="abc" width={15} height={18} />
+                        Johns Hopkins University
+                      </Typography.Paragraph>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div className="players-list">
+                <Typography.Title level={3}>
+                Popular Players
+                </Typography.Title>
+                <ul>
+                  <li>
+                    <Image
+                      src="/plyer-b.png"
+                      alt="abc"
+                      width={80}
+                      height={80}
+                    />
+                    <div className="ml-2">
+                      <Typography.Title level={5}>
+                        <span>Ralph</span> Edwards <i>99.3</i>
+                      </Typography.Title>
+                      <Typography.Paragraph>
+                        Senior Team Manager
+                      </Typography.Paragraph>
+                      <Typography.Paragraph>
+                        <Image src="/b.svg" alt="abc" width={15} height={18} />
+                        Johns Hopkins University
+                      </Typography.Paragraph>
+                    </div>
+                  </li>
+                  <li>
+                    <Image
+                      src="/plyer-b.png"
+                      alt="abc"
+                      width={80}
+                      height={80}
+                    />
+                    <div className="ml-2">
+                      <Typography.Title level={5}>
+                        <span>Ralph</span> Edwards <i>99.3</i>
+                      </Typography.Title>
+                      <Typography.Paragraph>
+                        Senior Team Manager
+                      </Typography.Paragraph>
+                      <Typography.Paragraph>
+                        <Image src="/b.svg" alt="abc" width={15} height={18} />
+                        Johns Hopkins University
+                      </Typography.Paragraph>
+                    </div>
+                  </li>
+                  <li>
+                    <Image
+                      src="/plyer-b.png"
+                      alt="abc"
+                      width={80}
+                      height={80}
+                    />
+                    <div className="ml-2">
+                      <Typography.Title level={5}>
+                        <span>Ralph</span> Edwards <i>99.3</i>
+                      </Typography.Title>
+                      <Typography.Paragraph>
+                        Senior Team Manager
+                      </Typography.Paragraph>
+                      <Typography.Paragraph>
+                        <Image src="/b.svg" alt="abc" width={15} height={18} />
+                        Johns Hopkins University
+                      </Typography.Paragraph>
+                    </div>
+                  </li>
+                  <li>
+                    <Image
+                      src="/plyer-b.png"
+                      alt="abc"
+                      width={80}
+                      height={80}
+                    />
+                    <div className="ml-2">
+                      <Typography.Title level={5}>
+                        <span>Ralph</span> Edwards <i>99.3</i>
+                      </Typography.Title>
+                      <Typography.Paragraph>
+                        Senior Team Manager
+                      </Typography.Paragraph>
+                      <Typography.Paragraph>
+                        <Image src="/b.svg" alt="abc" width={15} height={18} />
+                        Johns Hopkins University
+                      </Typography.Paragraph>
+                    </div>
+                  </li>
+                  <li>
+                    <Image
+                      src="/plyer-b.png"
+                      alt="abc"
+                      width={80}
+                      height={80}
+                    />
+                    <div className="ml-2">
+                      <Typography.Title level={5}>
+                        <span>Ralph</span> Edwards <i>99.3</i>
+                      </Typography.Title>
+                      <Typography.Paragraph>
+                        Senior Team Manager
+                      </Typography.Paragraph>
+                      <Typography.Paragraph>
+                        <Image src="/b.svg" alt="abc" width={15} height={18} />
+                        Johns Hopkins University
+                      </Typography.Paragraph>
+                    </div>
+                  </li>
+                  <li>
+                    <Image
+                      src="/plyer-b.png"
+                      alt="abc"
+                      width={80}
+                      height={80}
+                    />
+                    <div className="ml-2">
+                      <Typography.Title level={5}>
+                        <span>Ralph</span> Edwards <i>99.3</i>
+                      </Typography.Title>
+                      <Typography.Paragraph>
+                        Senior Team Manager
+                      </Typography.Paragraph>
+                      <Typography.Paragraph>
+                        <Image src="/b.svg" alt="abc" width={15} height={18} />
+                        Johns Hopkins University
+                      </Typography.Paragraph>
+                    </div>
+                  </li>
+                  <li>
+                    <Image
+                      src="/plyer-b.png"
+                      alt="abc"
+                      width={80}
+                      height={80}
+                    />
+                    <div className="ml-2">
+                      <Typography.Title level={5}>
+                        <span>Ralph</span> Edwards <i>99.3</i>
+                      </Typography.Title>
+                      <Typography.Paragraph>
+                        Senior Team Manager
+                      </Typography.Paragraph>
+                      <Typography.Paragraph>
+                        <Image src="/b.svg" alt="abc" width={15} height={18} />
+                        Johns Hopkins University
+                      </Typography.Paragraph>
+                    </div>
+                  </li>
+                  <li>
+                    <Image
+                      src="/plyer-b.png"
+                      alt="abc"
+                      width={80}
+                      height={80}
+                    />
+                    <div className="ml-2">
+                      <Typography.Title level={5}>
+                        <span>Ralph</span> Edwards <i>99.3</i>
+                      </Typography.Title>
+                      <Typography.Paragraph>
+                        Senior Team Manager
+                      </Typography.Paragraph>
+                      <Typography.Paragraph>
+                        <Image src="/b.svg" alt="abc" width={15} height={18} />
+                        Johns Hopkins University
+                      </Typography.Paragraph>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Flex>
+  );
 }
