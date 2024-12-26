82f1f771ad1666365b596134d449cc6e52cb7bdf
Muhammad Farhan <m.farhankz@gmail.com>
2024-12-26 18:04:56 +0500

    second page done

--- src/app/(dashboard)/_components/PlayerInformation.tsx
+++ src/app/(dashboard)/_components/PlayerInformation.tsx
@@ -251,6 +251,162 @@ const columns: TableColumnsType<DataType> = [
   },
 ];
 
+interface GameLog {
+  key: string;
+  date: string;
+  res: string;
+  cmp: number;
+  att: number;
+  cmp2: number;
+  yds: number;
+  avg: number;
+  td: number;
+  int: number;
+  lng: number;
+  sack: number;
+}
+
+const gameLog: GameLog[] = [
+  {
+    key: "1",
+    date: "Sun 10/20",
+    res: "28-20",
+    cmp: 6065,
+    att: 6690,
+    cmp2: 5948,
+    yds: 3536,
+    avg: 8811,
+    td: 1784,
+    int: 9261,
+    lng: 8829,
+    sack: 6025,
+  },
+  {
+    key: "2",
+    date: "Mon 10/7",
+    res: "11-29",
+    cmp: 5560,
+    att: 3933,
+    cmp2: 1577,
+    yds: 8861,
+    avg: 5560,
+    td: 8861,
+    int: 8861,
+    lng: 6025,
+    sack: 5045,
+  },
+  {
+    key: "3",
+    date: "Sun 9/29",
+    res: "29-30",
+    cmp: 5560,
+    att: 3933,
+    cmp2: 1577,
+    yds: 8861,
+    avg: 5560,
+    td: 8861,
+    int: 8861,
+    lng: 6025,
+    sack: 5045,
+  },
+  {
+    key: "4",
+    date: "Sun 9/22",
+    res: "22-20",
+    cmp: 5560,
+    att: 3933,
+    cmp2: 1577,
+    yds: 8861,
+    avg: 5560,
+    td: 8861,
+    int: 8861,
+    lng: 6025,
+    sack: 5045,
+  },
+  {
+    key: "5",
+    date: "Thu 9/5",
+    res: "28-30",
+    cmp: 5560,
+    att: 3933,
+    cmp2: 1577,
+    yds: 8861,
+    avg: 5560,
+    td: 8861,
+    int: 8861,
+    lng: 6025,
+    sack: 5045,
+  },
+];
+
+const gameLogColumns = [
+  {
+    title: "Date",
+    dataIndex: "date",
+    key: "date",
+  },
+  {
+    title: "Result",
+    dataIndex: "res",
+    key: "res",
+    render: (_: unknown, record: GameLog) => (
+      <div className="flex items-center">
+        {record.res && (
+          <span className="flex justify-center items-center h-5 w-5 bg-[#2BB650] mr-2">
+            <Image src="/w.svg" alt={record.res} width={16} height={13} />
+          </span>
+        )}
+        {record.res}
+      </div>
+    ),
+  },
+  {
+    title: "CMP",
+    dataIndex: "cmp",
+    key: "cmp",
+  },
+  {
+    title: "ATT",
+    dataIndex: "att",
+    key: "att",
+  },
+  {
+    title: "CMP2",
+    dataIndex: "cmp2",
+    key: "cmp2",
+  },
+  {
+    title: "YDS",
+    dataIndex: "yds",
+    key: "yds",
+  },
+  {
+    title: "AVG",
+    dataIndex: "avg",
+    key: "avg",
+  },
+  {
+    title: "TD",
+    dataIndex: "td",
+    key: "td",
+  },
+  {
+    title: "INT",
+    dataIndex: "int",
+    key: "int",
+  },
+  {
+    title: "LNG",
+    dataIndex: "lng",
+    key: "lng",
+  },
+  {
+    title: "SACK",
+    dataIndex: "sack",
+    key: "sack",
+  },
+];
+
 const Bio = () => (
   <div className="bio">
     <h3>Contact Information</h3>
@@ -318,7 +474,7 @@ const Bio = () => (
 
     <h3>College Roster Bio</h3>
     <div className="px-3">
-      <p>
+      <p className="my-3">
         Sed ut perspiciatis unde omnis iste natus error sit voluptatem
         accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
         illo inventore veritatis et quasi architecto beatae vitae dicta sunt
@@ -335,36 +491,167 @@ const Bio = () => (
 const Stats = () => (
   <div>
     <div className="mb-10">
-      <h3>Passing <span>89%</span></h3>
+      <h3>
+        Passing <span>89%</span>
+      </h3>
       <Table dataSource={dataSource} columns={columns} pagination={false} />
     </div>
     <div className="mb-10">
-      <h3>Defense <span>49%</span></h3>
+      <h3>
+        Defense <span>49%</span>
+      </h3>
       <Table dataSource={dataSource} columns={columns} pagination={false} />
     </div>
     <div>
-      <h3>Receiving <span>69%</span></h3>
+      <h3>
+        Receiving <span>69%</span>
+      </h3>
       <Table dataSource={dataSource} columns={columns} pagination={false} />
     </div>
   </div>
 );
 const GameLog = () => (
   <div>
-    <h3>Welcome to GameLog</h3>
-    <p>This is the GameLog tab content.</p>
+    <div className="mb-10">
+      <h3>Game Log</h3>
+      <div className="flex items-center my-3">
+        <Image
+          className="mr-1"
+          src={"/nfl.svg"}
+          alt={""}
+          width={29}
+          height={40}
+        />
+        <h4>NFL 2024 Fall Season</h4>
+      </div>
+      <Table dataSource={gameLog} columns={gameLogColumns} pagination={false} />{" "}
+    </div>
+    <div>
+      <h3>Defence</h3>
+      {/* <Table dataSource={gameLog} columns={gameLogColumns} pagination={false} /> */}
+      <Table dataSource={gameLog} columns={gameLogColumns} pagination={false} />
+    </div>
   </div>
 );
 
 const Survey = () => (
   <div>
-    <h2>Welcome to Survey</h2>
-    <p>This is the Survey tab content.</p>
+    <div>
+    <h3>Why they are leaving?</h3>
+    <div className="flex items-center survey mb-5">
+      <i className="icon-arrow-right text-2xl mr-2"></i>
+      <div>
+        <span>In their own words</span>
+        <h6>My school fontbonne University is closing at the end of summer 2025 so I am looking to play my senior year 5th year</h6>
+      </div>
+    </div>
+    <div className="flex items-center survey mb-5">
+      <i className="icon-arrow-right mr-2"></i>
+      <div>
+        <span>Desire a higher level</span>
+        <h6>Major Reason</h6>
+      </div>
+    </div>
+    </div>
+    <div>
+    <h3>What they are looking for?</h3>
+    <div className="flex items-center survey mb-5">
+      <i className="icon-arrow-right text-2xl mr-2"></i>
+      <div>
+        <span>In their own words</span>
+        <h6>Strong academic program and student academic support student life</h6>
+      </div>
+    </div>
+    <div className="flex items-center survey mb-5">
+      <i className="icon-arrow-right mr-2"></i>
+      <div>
+        <span>Are you open to walking on at a top program?</span>
+        <h6>I would consider it for the right school</h6>
+      </div>
+    </div>
+    <div className="flex items-center survey mb-5">
+      <i className="icon-arrow-right mr-2"></i>
+      <div>
+        <span>Preferred position</span>
+        <h6>CenterBack</h6>
+      </div>
+    </div>
+    <div className="flex items-center survey mb-5">
+      <i className="icon-arrow-right mr-2"></i>
+      <div>
+        <span>Ideal division</span>
+        <h6>D1 High Major, D1 Mid Major, D1 Low Major, D2/NAIA, D3</h6>
+      </div>
+    </div>
+    <div className="flex items-center survey mb-5">
+      <i className="icon-arrow-right mr-2"></i>
+      <div>
+        <span>Full scholarship only</span>
+        <h6>No</h6>
+      </div>
+    </div>
+    <div className="flex items-center survey mb-5">
+      <i className="icon-arrow-right mr-2"></i>
+      <div>
+        <span>Cost vs academic reputation</span>
+        <h6>Cost and academic reputation are equally important</h6>
+      </div>
+    </div>
+    <div className="flex items-center survey mb-5">
+      <i className="icon-arrow-right mr-2"></i>
+      <div>
+        <span>Winning vs location</span>
+        <h6>Winning tradition and location are equally important</h6>
+      </div>
+    </div>
+    <div className="flex items-center survey mb-5">
+      <i className="icon-arrow-right mr-2"></i>
+      <div>
+        <span>Playing time vs championship</span>
+        <h6>Personal impact and the team competing for the national championship are equally important</h6>
+      </div>
+    </div>
+    </div>
   </div>
 );
 const Notes = () => (
   <div>
-    <h2>Welcome to Notes</h2>
-    <p>This is the Notes tab content.</p>
+    <h3>Notes</h3>
+    <div>
+    <input className="border w-full my-4 p-2 text-lg relative" type="text" name="" id="" placeholder="Write new..." />
+    <i className="icon-send-1 absolute text-lg"></i>
+    </div>
+   <div className="notes">
+    <div className="flex">
+        <span className="h-9 w-9 jm mr-3 mt-3">JM</span>
+        <div className="w-full">
+         <div className="flex items-center justify-between">
+         <h6 className="w-[max-content]">Jason Masum</h6>
+         <span className="flex items-center">Edited  04/19/2024  4:59 PM <i className="icon-menu-1 ml-1"></i></span>
+         </div>
+            <p className="m-0">Adding a subtle animation here could help highlight important actions and make it more engaging.</p>
+        </div>
+    </div>
+    <hr />
+    <div className="flex">
+        <span className="h-9 w-9 jm mr-3 mt-3">JM</span>
+        <div className="w-full">
+         <div className="flex items-center justify-between">
+         <h6 className="w-[max-content]">Jason Masum</h6>
+         <span className="flex items-center">Edited  04/19/2024  4:59 PM <i className="icon-menu-1 ml-1"></i></span>
+         </div>
+            <p className="m-0">I&apos;m a fan! <br /> These work better than i thought they would. I&apos;ve never been big into the Kardashian brands so i was hesitant to try, but I&apos;m glad I did. I take two capsules with breakfast and i don&apos;t have the urge to snack throughout the day. Also has me eating lunch later and eating something small for dinner. I&apos;m still on my first month but did the subscription for three months for the discount. I&apos;ve got there</p>
+        </div>
+    </div>
+    <hr />
+    <div className="flex">
+        <span className="h-9 w-9 jm mr-3 mt-3">AC</span>
+        <div className="w-full">
+         <h6 className="w-[max-content]">Alex Cook</h6>
+            <p className="m-0">These work better than i thought they would. I&apos;ve never been big into the Kardashian brands so i was hesitant to try, but I&apos;m glad I did. I take two capsules with breakfast and i don&apos;t have the urge to snack throughout the day. Also has me eating lunch.</p>
+        </div>
+    </div>
+   </div>
   </div>
 );
 
